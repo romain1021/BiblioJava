a00301d7b6a9a9cc0b4728207736383916c920eb
romain1021 <150360051+romain1021@users.noreply.github.com>
2025-09-18 15:37:01 +0200

    Add initial Biblio and Livre classes

---
+++ Biblio.java
@@ -0,0 +1,31 @@
+public class Biblio {
+    
+    public static void main(String[] args) {
+        boolean isRunning = true;
+        while (isRunning) {
+            //interaction utilisateur
+        }    
+        
+        
+        public void [] updateBiblio(Biblio biblio) {
+            //recupère lis dans le fichier CSV pour recuperer les livre dedans et renvoie un tableau d'objet de livre livre
+        }
+
+        public void addLivre(Livre livre) {
+            //ajoute un livre dans le fichier CSV
+        }
+
+        public void deleteLivre(int id) {
+            //supprime un livre dans le fichier CSV en fonction de son id
+        }
+
+        public void chercherLivre(String nom) {
+            //cherche un livre dans le fichier CSV en fonction de son nom, renvoie un tableau d'objet de livre correspondant
+        }
+
+        public void afficherLivreListe(){
+            // affiche les tous les elements des objet livre dans le tableau d'objet de livre.
+        }
+
+    
+}
---
+++ Livre.java
@@ -0,0 +1,65 @@
+public class Livre {
+    private int id;
+    private String auteur;
+    private String nom;
+    private String date;
+    private int page;
+    private String descriptif;
+
+    public Livre(int id, String auteur, String nom, String date, int page, String descriptif) {
+        this.id = id;
+        this.auteur = auteur;
+        this.nom = nom;
+        this.date = date;
+        this.page = page;
+        this.descriptif = descriptif;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getAuteur() {
+        return auteur;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public String getDescriptif() {
+        return descriptif;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setAuteur(String auteur) {
+        this.auteur = auteur;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public void setDescriptif(String descriptif) {
+        this.descriptif = descriptif;
+    }
+}
